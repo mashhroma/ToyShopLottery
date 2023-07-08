0d1cb20f06e5a6c32e6d3bdb600350d596bed9fd
Mashhroma <mashh@mail.ru>
2023-07-08 23:47:50 +0300

    improved programm

--- ToyCollection.java
+++ ToyCollection.java
@@ -30,53 +30,46 @@ public class ToyCollection {
     }
 
     public void runLottery() {
-        double possibility = new Random().nextInt(100);
-        double start = 0;
-        collection.sort(null);
-        for (Toy toy : collection) {
-            if (possibility >= start && possibility < (start + toy.chance)) {
-                prizeQueue.add(new Prize(toy.name));
-                if (toy.quantity > 1)
-                    toy.setQuantity(toy.quantity - 1);
-                else
-                    collection.remove(toy);
-                System.out.printf("Вы выиграли игрушку: %s\n", toy.name);
+        if (collection.isEmpty()) {
+            System.out.println("Невозможно провести розыгрыш. В автомате нет игрушек.");
+        } else {
+            double possibility = new Random().nextInt(100);
+            double start = 0;
+            collection.sort(null);
+            for (Toy toy : collection) {
+                if (possibility >= start && possibility < (start + toy.chance)) {
+                    prizeQueue.add(new Prize(toy.name));
+                    if (toy.quantity > 1)
+                        toy.setQuantity(toy.quantity - 1);
+                    else
+                        collection.remove(toy);
+                    System.out.printf("Участник выиграл игрушку: %s\n", toy.name);
+                }
+                start += toy.chance;
             }
-            start += toy.chance;
+            setAllChances();
+            collection.sort(null);
         }
-        setAllChances();
-        collection.sort(null);
-        ;
     }
 
-    // public void addPrize(Toy prizeToy) {
-    // boolean available = false;
-    // for (Prize prize : prizeQueue) {
-    // if (prize.getId() == prizeToy.getId()) {
-    // prize.setQuantity(prize.quantity + 1);
-    // available = true;
-    // }
-    // }
-    // if (available == false) {
-    // prizeQueue.add(new Prize(prizeToy.getId(), prizeToy.name));
-    // }
-    // }
-
     public void getToys() {
-        Date date = new Date();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("E yyyy.MM.dd hh:mm:ss");
-        String fileName = "givenPrizes.txt";
-        Prize prize = prizeQueue.element();
-        try (FileWriter writer = new FileWriter(fileName, Charset.forName("utf-8"), true)) {
-            writer.write(dateFormat.format(date) + " ВЫДАН ПРИЗ: номер лота " + prize.getId() + ", игрушка - " + prize.name + "\n");
-            // writer.write("\"clientName\":\"" + prize.getId() + "\",\n");
-            // writer.write("\"clientName\":\"" + prize.name + "\",\n");
-            // writer.write("}\n");
-            writer.flush();
-            System.out.println(dateFormat.format(date) + " ВЫДАН ПРИЗ: номер лота " + prize.getId() + ", игрушка - " + prize.name + "\n");
-            prizeQueue.remove();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+        if (prizeQueue.isEmpty()) {
+            System.out.println("Нет игрушек, готовых к выдаче участникам. Нужно вначале провести розыгрыш призов.");
+        } else {
+            Date date = new Date();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("E yyyy.MM.dd hh:mm:ss");
+            String fileName = "prizesReport.txt";
+            Prize prize = prizeQueue.element();
+            try (FileWriter writer = new FileWriter(fileName, Charset.forName("utf-8"), true)) {
+                writer.write(dateFormat.format(date) + " ВЫДАН ПРИЗ: номер лота " + prize.getId() + ", игрушка - "
+                        + prize.name + "\n");
+                writer.flush();
+                System.out.println(dateFormat.format(date) + " ВЫДАН ПРИЗ: номер лота " + prize.getId() + ", игрушка - "
+                        + prize.name + ". Запись о выдаче внесена в файл.\n");
+                prizeQueue.remove();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
         }
     }
 
@@ -94,14 +87,16 @@ public class ToyCollection {
         if (prizeQueue.isEmpty()) {
             System.out.println("Нет игрушек, готовых к выдаче.");
         } else {
-            Map<String, Integer> prizes = new HashMap<>();;
+            Map<String, Integer> prizes = new HashMap<>();
+            ;
             for (Prize prize : prizeQueue) {
                 int count = 1;
-                if (prizes.containsKey(prize.name)) count = prizes.get(prize.name)+1;
+                if (prizes.containsKey(prize.name))
+                    count = prizes.get(prize.name) + 1;
                 prizes.put(prize.name, count);
             }
             for (var prize : prizes.entrySet()) {
-                System.out.printf("%s, количество %d\n", prize.getKey(), prize.getValue());
+                System.out.printf("- %s, количество %d\n", prize.getKey(), prize.getValue());
             }
         }
     }
